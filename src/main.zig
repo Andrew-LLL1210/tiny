f95b03184f1191d84067efaadc4c992657d3b61a
andrew-lll1210 <addadp1961@gmail.com>
2024-02-03 22:05:18 -0600

    make program return error code on error (wow)

--- src/main.zig
+++ src/main.zig
@@ -41,31 +41,9 @@ pub fn main() !void {
     };
 
     // Get listing from parser
-    const listing = parse.parse(source, &reporter, alloc) catch |err| switch (err) {
-        error.OutOfMemory => return err,
-        error.ReportedError => return,
-    };
+    const listing = try parse.parse(source, &reporter, alloc);
     defer alloc.free(listing);
 
     reporter.listing = listing;
-
-    //    var prev_line_no: usize = std.math.maxInt(usize);
-    //    for (listing) |item| {
-    //        if (item.line_no == prev_line_no) continue;
-    //        prev_line_no = item.line_no;
-    //
-    //        try stdout.print("{[ip]d:0>3} {[word]d: >6} line {[line_no]d: <4}", item);
-    //        defer stdout.writeByte('\n') catch {};
-    //
-    //        var lines = std.mem.splitScalar(u8, source, '\n');
-    //        for (1..item.line_no) |_| _ = lines.next();
-    //        if (lines.next()) |line| {
-    //            var comment_splitter = std.mem.splitScalar(u8, line, ';');
-    //            const inst = std.mem.trim(u8, comment_splitter.first(), &std.ascii.whitespace);
-    //            try stdout.print(" {s}", .{inst});
-    //        }
-    //    }
-
-    // run program
-    run.runMachine(listing, stdin, stdout, &reporter) catch {};
+    try run.runMachine(listing, stdin, stdout, &reporter);
 }
